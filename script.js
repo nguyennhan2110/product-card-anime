b1c1a956a69f33d0b26b44f6ef44b03dfb35b724
Nhan Nguyen <nguyen.nhan211020@gmail.com>
2023-06-09 01:16:11 +0700

    fix format

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Producto a favoritos
-$('.card .aFavs').click(function(){
-  $(this).parents('.card').toggleClass('esFav');
-})
+$(".card .aFavs").click(function () {
+  $(this).parents(".card").toggleClass("esFav");
+});
 //Producto al carrito
-$('.card .alCarrito').click(function(){
-  $(this).parents('.card').toggleClass('enCarrito');
-})
+$(".card .alCarrito").click(function () {
+  $(this).parents(".card").toggleClass("enCarrito");
+});
